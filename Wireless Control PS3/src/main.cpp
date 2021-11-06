c08cf7bcb4655fc887aef6da4d7badacfbe1a0ad
Stalkerfish <silva.allison2002@gmail.com>
2021-11-06 19:08:21 -0300

    add SoftwareSerial library to the project

--- Wireless Control PS3/src/main.cpp
+++ Wireless Control PS3/src/main.cpp
@@ -1,9 +1,31 @@
 #include <Arduino.h>
+#include <SoftwareSerial.h>
+
+SoftwareSerial bluetooth(2, 3); // RX, TX
 
 void setup() {
-  // put your setup code here, to run once:
+
+  Serial.begin(9600);   // inicia comunicação serial do arduino
+  bluetooth.begin(9600);    //inicia comunicação serial do bluetooth
+
+  pinMode(LED_BUILTIN, OUTPUT); //configura o led do pino 13 como saída
+
+  digitalWrite(LED_BUILTIN, LOW); // inicia o led em estado LOW
+
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  if (bluetooth.available() > 0){
+    int incomingByte = bluetooth.read();
+
+    if(incomingByte == 'H'){
+      digitalWrite(LED_BUILTIN, HIGH);
+      Serial.print("LED LIGADO!");
+    }
+    if (incomingByte == 'L'){
+      digitalWrite(LED_BUILTIN, LOW);
+      Serial.print("LED DESLIGADO!");
+    }
+    
+  }
 }
